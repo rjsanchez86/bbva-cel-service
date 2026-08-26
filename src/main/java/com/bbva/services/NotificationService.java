8c4bbfeef6419627ab88a5d0f55fc70d03ec90a0
robertodejesus.sanchez.acuna <robertodejesus.sanchez.acuna@bbva.com>
2026-08-26 09:29:16 -0600

    Separar metodos POST y PUT en NotificationService

--- src/main/java/com/bbva/services/NotificationService.java
+++ src/main/java/com/bbva/services/NotificationService.java
@@ -10,12 +10,26 @@ import javax.ws.rs.core.MediaType;
 @Path("services")
 public class NotificationService {
 
+    // Soporte para peticiones POST (Seguimiento de especificación de código Java)
     @POST
+    @Path("notification")
+    @Consumes({MediaType.APPLICATION_JSON})
+    @Produces({MediaType.APPLICATION_JSON})
+    public NotificationDTO notificationProcessPost(NotificationDTO notificationDTO) {
+        return process(notificationDTO);
+    }
+
+    // Soporte para peticiones PUT (Seguimiento de anexo formal BBVA)
     @PUT
     @Path("notification")
     @Consumes({MediaType.APPLICATION_JSON})
     @Produces({MediaType.APPLICATION_JSON})
-    public NotificationDTO notificationProcess(NotificationDTO notificationDTO) {
+    public NotificationDTO notificationProcessPut(NotificationDTO notificationDTO) {
+        return process(notificationDTO);
+    }
+
+    // Lógica compartida
+    private NotificationDTO process(NotificationDTO notificationDTO) {
         if (notificationDTO.getCodresp() == null || notificationDTO.getCodresp().isEmpty()) {
             notificationDTO.setCodresp("00");
         }
